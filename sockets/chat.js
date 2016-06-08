97fdb3f45a0fa7045f7e1642bb7ddca31f4e8564
mariofalcao <mariofalcao>
2016-06-08 11:57:32 -0300

    'commit'

--- sockets/chat.js
+++ sockets/chat.js
@@ -7,9 +7,16 @@ module.exports = function(io) {
 
 // usernames which are currently connected to the chat
 var usernames = {};
-var numUsers = 0;
+var numUserPerRoom = {};
+var numUsersTotal = 0;
 // rooms which are currently available in chat
 var rooms = ['room1','room2','room3'];
+
+//incializando as salas com numeros de usuario = 0
+for (var i = rooms.length - 1; i >= 0; i--) {
+  var roomName = rooms[i];
+  numUserPerRoom[roomName] = 0;
+};
 
 io.sockets.on('connection', function (socket) {
 
@@ -38,27 +45,28 @@ var session = socket.handshake.session;
     // we store the username in the socket session for this client
     socket.username = username;
 
+    numUserPerRoom[socket.room] =  numUserPerRoom[socket.room] + 1;
+
     // add the client's username to the global list
     usernames[socket.id] = username;
-    ++numUsers;
+    ++numUsersTotal;
     addedUser = true;
     socket.emit('login', {
-      numUsers: numUsers
+      numUsersTotal: numUsersTotal
     });
 
     // echo globally (all clients) that a person has connected
     socket.broadcast.to('room1').emit('user joined', {
       username: socket.username,
-      numUsers: numUsers
+      numUsersTotal: numUsersTotal
     });
 
     socket.emit('updaterooms', rooms, 'room1');
 
+    //var usernamesAux = JSON.parse(JSON.stringify(usernames));
+    //io.sockets.emit envia mensagem pra todo mundo, inclusive pro SENDER
 
-    var usernamesAux =  [].concat(usernames || []);
-    console.log(usernamesAux)
-
-    socket.broadcast.emit('updateusers', usernames);
+    io.sockets.emit('updateusers', usernames);
 
   });
 
@@ -82,20 +90,20 @@ var session = socket.handshake.session;
 
     if (addedUser) {
       delete usernames[socket.id];
-      --numUsers;
+      --numUsersTotal;
+      numUserPerRoom[socket.room] = numUserPerRoom[socket.room] - 1;
 
       // update list of users in chat, client-side
-      socket.broadcast.emit('updateusers', usernames);
+      io.sockets.emit('updateusers', usernames);
 
       // echo globally that this client has left
-      socket.broadcast.emit('user left', {
+      socket.broadcast.to(socket.room).emit('user left', {
         username: socket.username,
-        numUsers: numUsers
+        numUsersTotal: numUserPerRoom[socket.room]
       });
 
       //eliminando usuario do room respectivo
       socket.leave(socket.room);
-
     }
   });
 
@@ -105,24 +113,58 @@ var session = socket.handshake.session;
     // leave the current room (stored in session)
     socket.leave(socket.room);
 
+    //atualizando quantidade de usuarios sala antiga
+    numUserPerRoom[socket.room] = numUserPerRoom[socket.room] - 1;
+
+    //atualizando quantidade de usuarios sala nova
+    numUserPerRoom[newroom] = numUserPerRoom[newroom] + 1;
+
     // join new room, received as function parameter
     socket.join(newroom);
 
     // sent message to OLD room
     socket.broadcast.to(socket.room).emit('user left', {
         username: socket.username,
-        numUsers: numUsers
+        numUsersTotal: numUserPerRoom[socket.room]
       });
 
     // update socket session room title
     socket.room = newroom;
     socket.broadcast.to(newroom).emit('user joined', {
       username: socket.username,
-      numUsers: numUsers
+      numUsersTotal: numUserPerRoom[newroom]
     });
 
     socket.emit('updaterooms', rooms, newroom);
   });
+
+  //FUNCOES DE USER
+  socket.on('switchUser', function(id){
+
+    console.log('recebendo id' +id)
+
+      if(socket.room != undefined || socket.room != ''){
+        // leave the current room (stored in session)
+        socket.leave(socket.room);
+        //atualizando quantidade de usuarios sala antiga
+        numUserPerRoom[socket.room] = numUserPerRoom[socket.room] - 1;               
+        // sent message to OLD room
+        socket.broadcast.to(socket.room).emit('user left', {
+          username: socket.username,
+          numUsersTotal: numUserPerRoom[socket.room]
+        });
+        
+        // update socket session room title
+        socket.room = '';
+      }
+  
+      socket.to(id).emit('new message', {
+        username: socket.username,
+        message: "mensagem privada"
+      });
+   
+  });
+
 
 });
 
